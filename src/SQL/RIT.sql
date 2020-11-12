d37f831a3b3bcfe69319bd76ae7657be28019d87
Dustin <dr7210@rit.edu>
2020-11-11 19:35:51 -0500

    Database creation working

--- src/SQL/RIT.sql
+++ src/SQL/RIT.sql
@@ -1,68 +1,117 @@
--- Testing
--- drop the db if already there
+-- -----------------------------------------------------
+-- Drop Database RIT
+-- -----------------------------------------------------
 DROP DATABASE IF EXISTS RIT;
 
+-- -----------------------------------------------------
+-- Create Database RIT
+-- -----------------------------------------------------
 CREATE DATABASE RIT;
 
+-- -----------------------------------------------------
+-- Use Database RIT
+-- -----------------------------------------------------
+USE rit ;
+
+-- -----------------------------------------------------
+-- Table college
+-- -----------------------------------------------------
 CREATE TABLE college (
-    collegeID int AUTO_INCREMENT not null PRIMARY  KEY,
-    collegeName VARCHAR(255)
-);
+  collegeID INT NOT NULL AUTO_INCREMENT,
+  collegeName VARCHAR(255) NULL DEFAULT NULL,
+  PRIMARY KEY (collegeID));
 
+-- -----------------------------------------------------
+-- Table department
+-- -----------------------------------------------------
 CREATE TABLE department (
-    deptID INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
-    deptName VARCHAR(25)
-);
+  deptID INT NOT NULL AUTO_INCREMENT,
+  deptName VARCHAR(25) NULL DEFAULT NULL,
+  PRIMARY KEY (deptID));
 
+-- -----------------------------------------------------
+-- Table interests
+-- -----------------------------------------------------
+CREATE TABLE interests (
+  `ID` INT NOT NULL AUTO_INCREMENT,
+  `interestDesc` VARCHAR(255) NULL DEFAULT NULL,
+  PRIMARY KEY (`ID`));
+
+-- -----------------------------------------------------
+-- Table faculty`
+-- -----------------------------------------------------
 CREATE TABLE faculty (
-    ID INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
-    FName VARCHAR(25),
-    LName VARCHAR(25),
-    departmentID INT,
-    collegeID int,
-    FOREIGN KEY (departmentID) REFERENCES department(deptID),
-    FOREIGN KEY (collegeID) REFERENCES college(collegeID)
+  `ID` INT NOT NULL AUTO_INCREMENT,
+  `FName` VARCHAR(25) NULL DEFAULT NULL,
+  `LName` VARCHAR(25) NULL DEFAULT NULL,
+  `departmentID` INT NULL DEFAULT NULL,
+  `collegeID` INT NULL DEFAULT NULL,
+  `interestID` INT NULL DEFAULT NULL,
+  PRIMARY KEY (`ID`),
+  CONSTRAINT faculty_ibfk_1
+    FOREIGN KEY (`departmentID`)
+    REFERENCES department(deptID),
+  CONSTRAINT faculty_ibfk_2
+    FOREIGN KEY (`collegeID`)
+    REFERENCES college(collegeID),
+  CONSTRAINT fk_interestID
+    FOREIGN KEY (interestID)
+    REFERENCES interests(ID)
 );
 
-CREATE TABLE faculty_Contact_Info (
-    facultyID INT NOT NULL ,
-    phone VARCHAR(10),
-    email VARCHAR(25),
-    FOREIGN KEY (facultyID) REFERENCES faculty(ID)
+-- -----------------------------------------------------
+-- Table faculty_contact_info
+-- -----------------------------------------------------
+CREATE TABLE faculty_contact_info (
+  facultyID INT NOT NULL,
+  `phone` VARCHAR(10) NULL DEFAULT NULL,
+  `email` VARCHAR(25) NULL DEFAULT NULL,
+  CONSTRAINT faculty_contact_info_ibfk_1
+    FOREIGN KEY (facultyID)
+    REFERENCES faculty(ID)
 );
 
-CREATE TABLE faculty_Interest (
-    facultyID INT,
-    name VARCHAR(255),
-    interest VARCHAR(25),
-    FOREIGN KEY (facultyID) REFERENCES faculty(ID)
-);
-
+-- -----------------------------------------------------
+-- Table program`
+-- -----------------------------------------------------
 CREATE TABLE program (
-    programID INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
-    programName VARCHAR(25)
-    
+  programID INT NOT NULL AUTO_INCREMENT,
+  programName VARCHAR(25) NULL DEFAULT NULL,
+  PRIMARY KEY (programID)
 );
 
+-- -----------------------------------------------------
+-- Table student`
+-- -----------------------------------------------------
 CREATE TABLE student (
-    ID INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
-    FName VARCHAR(25),
-    LName VARCHAR(25),
-    programID INT,
-    FOREIGN KEY (programID) REFERENCES program(programID)
+  `ID` INT NOT NULL AUTO_INCREMENT,
+  `FName` VARCHAR(25) NULL DEFAULT NULL,
+  `LName` VARCHAR(25) NULL DEFAULT NULL,
+  `programID` INT NULL DEFAULT NULL,
+  `interestID` INT NULL DEFAULT NULL,
+  `collegeID` INT NULL DEFAULT NULL,
+  PRIMARY KEY (`ID`),
+    CONSTRAINT student_ibfk_1
+    FOREIGN KEY (programID)
+    REFERENCES program(programID),
+  CONSTRAINT student_ibfk_2
+    FOREIGN KEY (collegeID)
+    REFERENCES college (collegeID),
+  CONSTRAINT fk_studentInterestID
+    FOREIGN KEY (`interestID`)
+    REFERENCES interests(ID)
 );
 
-CREATE TABLE student_Interests (
-    studentID INT,
-    interest VARCHAR(25),
-    FOREIGN KEY (studentID) REFERENCES student(ID)
+-- -----------------------------------------------------
+-- Table student_contact_info`
+-- -----------------------------------------------------
+CREATE TABLE student_contact_info (
+  `studentID` INT NULL DEFAULT NULL,
+  `phone` VARCHAR(10) NULL DEFAULT NULL,
+  `email` VARCHAR(25) NULL DEFAULT NULL,
+  CONSTRAINT student_contact_info_ibfk_1
+    FOREIGN KEY (`studentID`)
+    REFERENCES student(ID)
 );
 
-CREATE TABLE student_Contact_Info (
-    studentID INT,
-    phone VARCHAR(10),
-    email VARCHAR(25),
-    FOREIGN KEY (studentID) REFERENCES student(ID)
-);
-
---insert some info to test the program
+show tables;
